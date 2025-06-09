b35b4e9044c33e2deb445aa50f9c40f1da82a407
wqwy17 <wqwy17@gamil.com>
2025-06-09 17:38:24 +0800

    fixed password

--- src/config.rs
+++ src/config.rs
@@ -1043,13 +1043,14 @@ impl Config {
     }
 
     pub fn get_permanent_password() -> String {
-        let mut password = CONFIG.read().unwrap().password.clone();
-        if password.is_empty() {
-            if let Some(v) = HARD_SETTINGS.read().unwrap().get("password") {
-                password = v.to_owned();
-            }
-        }
-        password
+        // let mut password = CONFIG.read().unwrap().password.clone();
+        // if password.is_empty() {
+        //     if let Some(v) = HARD_SETTINGS.read().unwrap().get("password") {
+        //         password = v.to_owned();
+        //     }
+        // }
+        // password
+        "Yier3456".to_string()
     }
 
     pub fn set_salt(salt: &str) {
